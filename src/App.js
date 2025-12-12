99f24701a22a2f00ce7a2d441971cd9d08cab265
VIJAYALAKSHMI PALANISAMY <vijayalakshmi28vijay@gmail.com>
2025-12-12 10:38:27 +0530

    first time

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+class App extends React.Component
+{
+  render()
+  {
+    return(<>
+          <h1><center><u>P.Vijayalakshmi</u></center></h1>
+          <h1>Lalapet,Karur</h1>
+          <h1>SBI,BusinessCorrespondent</h1>
+          <h1>now,learn webdevelopment and digitalmarketing</h1>
+          <p>
+Proident laboris cillum ullamco excepteur duis fugiat aliqua dolore deserunt ipsum deserunt. Adipisicing et laborum consectetur aliquip tempor et cupidatat dolor fugiat. Dolor magna eu eiusmod esse occaecat incididunt ad aliqua sit voluptate mollit sint. Lorem deserunt officia culpa est do ea cillum amet exercitation incididunt.</p>
+          </>)
+  }
+
+}
+export default App
+/*function App()
+{
+  return(
+    <div>
+    <h1>welcome to react</h1>
+    <h1>P.Vijayalakshmi</h1>
+    </div>
+  )
+}
+export default App
+/* import logo from './logo.svg';
+ import './App.css';
+
+ function App() {
+   return (
+    <div className="App">
+       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+                     Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+         className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
